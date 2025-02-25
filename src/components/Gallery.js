4ea04133bd5b69a2bfd349d26348ceaf0f58f9c2
StevenVC-P <steven.vancourt@gmail.com>
2025-02-25 17:10:56 -0600

    Sonic Add

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,12 @@
 // src/components/Gallery.js
 import React, { useState } from "react";
 import "./Gallery.css";
-import artPieces from "../artPieces.json";
+import turtles from "../artPieces/Turtles.json";
+import sonic from "../artPieces/Sonic.json";
 
 const Gallery = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [activeTab, setActiveTab] = useState("ninjaTurtles");
 
   const openLightbox = (imageUrl) => {
     setSelectedImage(imageUrl);
@@ -14,21 +16,35 @@ const Gallery = () => {
     setSelectedImage(null);
   };
 
-  return (
-    <div className="gallery">
-      {artPieces.map((piece, index) => (
-        <div key={index} className="art-piece" onClick={() => openLightbox(piece.imageUrl)}>
-          <img src={require(`../${piece.imageUrl}`)} alt={piece.title} />
-          <h3 className="art-piece-title glow">{piece.title}</h3>
-        </div>
-      ))}
+  const renderImages = () => {
+    const images = activeTab === "ninjaTurtles" ? turtles : sonic;
+    return images.map((piece, index) => (
+      <div key={index} className="art-piece" onClick={() => openLightbox(piece.imageUrl)}>
+        <img src={require(`../${piece.imageUrl}`)} alt={piece.title} />
+        <h3 className="art-piece-title glow">{piece.title}</h3>
+      </div>
+    ));
+  };
 
-      {selectedImage && (
-        <div className="lightbox" onClick={closeLightbox}>
-          <img src={require(`../${selectedImage}`)} alt="Selected Art" />
+  return (
+      <div className="gallery">
+        <div className="tabs">
+          <button className={activeTab === "ninjaTurtles" ? "active" : ""} onClick={() => setActiveTab("ninjaTurtles")}>
+            Ninja Turtles
+          </button>
+          <button className={activeTab === "sonic" ? "active" : ""} onClick={() => setActiveTab("sonic")}>
+            Sonic
+          </button>
         </div>
-      )}
-    </div>
+        <div className="gallery-container">
+          {renderImages()}
+        </div>
+        {selectedImage && (
+          <div className="lightbox" onClick={closeLightbox}>
+            <img src={require(`../${selectedImage}`)} alt="Selected Art" />
+          </div>
+        )}
+      </div>
   );
 };
 
